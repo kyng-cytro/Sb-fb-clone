9bee10e35e8c1c94346e5d68a61f58dba57d2e93
Miles Batey <miles.batey@gmail.com>
2021-09-30 16:05:05 -0600

    final ize commit

---
+++ src/components/HomeComponents/PostDisplay.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div>
+        <p>Posts</p>
+      </div>
+      <div class="postDisplay">
+        <div class="square" v-for="post in posts" :key="post.name">
+           <PostSquare v-bind:post="post"/> 
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostSquare from './PostSquare.vue';
+
+const posts = [
+  {
+    imgSrc: "basket.jpg",
+    name: "Basket Weaving",
+    text: "Come join our Basket Weaving on firday",
+    date: "FRI, DEC 24 AT 5PM MST",
+    numInterested: 21,
+    numGoing: 52
+  },
+  {
+    imgSrc: "chiliCookOff.jpg",
+    name: "Chili Cookoff",
+    text: " Come try our chili",
+    date: "FRI, DEC 24 AT 5PM MST",
+    numInterested: "9.3K",
+    numGoing: 523
+  },
+  {
+    imgSrc: "library.jpg",
+    name: "Reading Club",
+    text: " Read your book in our nook",
+    date: "FRI, DEC 24 AT 5PM MST",
+    numInterested: 23,
+    numGoing: 7
+  },
+];
+
+
+
+export default {
+  name: "postDisplay",
+  components: {
+    PostSquare
+
+  },
+  data(){
+    return {posts}
+  },
+}
+
+</script>
+
+
+<style scoped>
+.postDisplay {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+.square {
+  padding: 5px;
+}
+
+</style>
---
+++ src/components/HomeComponents/PostSquare.vue
@@ -0,0 +1,95 @@
+<template>
+        <div class="postSquare">
+          <img class="eventPic" :src="require('@/assets/images/EventImages/' + post.imgSrc)" />
+          <div class="info">
+          <strong id="date">{{post.date}}</strong>
+          <br />
+          <strong id="name">{{post.name}}</strong>
+          <br />
+          <strong id="text">{{post.text}}</strong>
+          <br />
+          <em>{{post.numInterested + " Interested · " + post.numGoing + " Going"}}</em>
+            <div>
+              <button type="button" v-on:click="handleInterestedClick" class="btn btn-secondary">Interested</button>
+              <button type="button" class="btn btn-secondary">Share</button>
+            </div>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: "post-square",
+  props: ["post"],
+  // data() {
+  //   return { deleted: false, accepted: false }
+  // },
+  methods: {
+    handleInterestedClick() {
+      console.log("You're intererested!");
+    },
+  }
+}
+</script>
+
+
+<style scoped>
+.postSquare {
+  /* box-shadow: 0px 0px 3px; */
+  width: 100%;
+  border-radius: 10px;
+  margin: 10%;
+  /* padding: 10%; */
+  box-shadow: 0px 0px 3px rgb(140, 140, 140);
+  display: flex;
+  flex-direction: column;
+}
+
+.eventPic {
+  width: 350px;
+  height: 180px;
+  object-fit: cover; /*This makes it so the image is cropped instead of squished */
+  border-radius: 10px 10px 0px 0px;
+}
+
+.info {
+  margin:10px;
+}
+
+#name:hover {
+  text-decoration: underline;
+}
+
+button {
+  width: 90%;
+  margin: 2%;
+  font-weight: bold;
+  font-size: .9em;
+}
+
+.btn-secondary {
+  color: black;
+  background-color: hsl(0, 0%, 92%);
+  border: 0px
+}
+
+.invisible {
+  color: white;
+  background-color: white;
+}
+
+#data {
+  color: rgb(111, 111, 111);
+  font-size: .9em;
+}
+
+#date {
+  font-size: 0.9em;
+}
+
+em {
+  color: rgb(111, 111, 111);
+  font-size: .9em;
+}
+
+</style>
