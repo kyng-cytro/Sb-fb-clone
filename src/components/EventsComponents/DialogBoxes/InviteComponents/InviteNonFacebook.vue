24cdce2aa44b94afdf15c8e4fc3142677b2f7f5f
AnsonSavage <ansonsavage@gmail.com>
2021-10-15 13:40:49 -0600

    Began coding invite nonfacebook users

--- src/components/EventsComponents/DialogBoxes/InviteComponents/InviteNonFacebook.vue
+++ src/components/EventsComponents/DialogBoxes/InviteComponents/InviteNonFacebook.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
+        <h3>Invite non Facebook users</h3>
         <ul>
         </ul>
     </div>
 </template>
 
 <script>
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+Vue.use(VueMaterial)
     export default {
         props: ['invitedByEmail', 'invitedByTest'],
         
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
