9195b9b407f3b8d6887618a2f21cc85f1f38ad13
AnsonSavage <ansonsavage@gmail.com>
2021-10-15 10:54:27 -0600

    Vuex test properly adding data to the store

---
+++ src/components/EventsComponents/VuexTest.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <input v-model="form.name">
+    <button @click="addFriend">Add friend</button>
+    <li>
+        
+    </li>
+</div>
+  
+</template>
+
+<script>
+import Friend from "@/classes/friend.js";
+
+export default {
+    data() {
+        return {
+            form: {
+                name: '',
+            }
+        }
+    },
+    methods: {
+        addFriend() {
+            Friend.insert({ data: this.form});
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
