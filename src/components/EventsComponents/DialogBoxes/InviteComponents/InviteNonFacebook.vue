cb53fb0b3198783d90e42583a301240cbb474ccd
AnsonSavage <ansonsavage@gmail.com>
2021-10-16 14:55:10 -0600

    Vue Material imported into InviteNonFacebook component

--- src/components/EventsComponents/DialogBoxes/InviteComponents/InviteNonFacebook.vue
+++ src/components/EventsComponents/DialogBoxes/InviteComponents/InviteNonFacebook.vue
@@ -1,22 +1,36 @@
 <template>
-    <div>
+    <div class="container">
         <h3>Invite non Facebook users</h3>
+        <md-field>
+            <label>Enter name</label>
+            <md-input v-model="name"></md-input>
+            <span class="md-helper-text">Helper text</span>
+        </md-field>
         <ul>
         </ul>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 Vue.use(VueMaterial)
+
     export default {
-        props: ['invitedByEmail', 'invitedByTest'],
-        
+        data() {
+            return {
+                name: '',
+                email: '',
+                phoneNumber: '',
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+.container {
+    padding: 15px;
+}
 </style>
