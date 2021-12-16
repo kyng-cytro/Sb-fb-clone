dd49494d0b81297fda32432058627ffe19d4ff75
AnsonSavage <ansonsavage@gmail.com>
2021-12-16 16:01:58 -0700

    Creating Login in page!

---
+++ src/components/HomeComponents/LoginComponents/LoginPage.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="main">
+      <div class="facebookLogoHalf">
+        <img src="@/assets/images/LoginImages/Long_Logo.svg" />
+        <h3>Connect with friends and the world around you on Facebook.</h3>
+      </div>
+      <div class="LoginHalf">
+        <div class="LoginBox">
+          <textarea
+            autofocus
+            class="logInTextArea"
+            placeholder="Email or phone number"
+          ></textarea>
+          <textarea
+            autofocus
+            class="logInTextArea"
+            placeholder="Password"
+          ></textarea>
+          <button class="btn btn-primary">Log In</button>
+          <a>Forgot password?</a>
+        </div>
+        <br />
+        <p><strong>Create a page</strong> for a celebrity, band, or business</p>
+      </div>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped>
+  .main {
+    display: flex;
+    padding: 100px;
+  }
+
+  .LoginBox {
+    width: 400px;
+    height: 300px;
+    box-shadow: 0px 0px 10px rgb(177, 175, 175);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+
+  .logInTextArea {
+    border-radius: 6px;
+    font-size: 17px;
+    padding: 14px 16px;
+    width: 330px;
+    border: 1px solid #dddfe2;
+    color: #1d2129;
+    height: 55px;
+    line-height: 25px;
+    vertical-align: middle;
+    outline: 0.5px solid rgb(230, 230, 230);
+    margin: 15px;
+    resize: none;
+  }
+  .logInTextArea:focus {
+    border: 1px solid #81b3ff;
+  }
+  .logInTextArea:focus::placeholder {
+    color: rgb(187, 187, 187);
+  }
+  button {
+      font-size: 1.3em;
+      font-weight: bold;
+      border-radius: 6px;;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,68 @@
 <template>
-  <div class="home">
-     <HomeSidebar />
-      <div class = "content">
+  <div>
+    <div v-if="!userLoggedIn">
+      <LoginPage />
+    </div>
+    <div v-else class="home">
+      <HomeSidebar />
+      <div class="content">
         <CreatePost />
         <PostDisplay />
       </div>
 
-      <div class = "filter">
+      <div class="filter">
         <FacebookLiteFilters />
       </div>
-
-      
+    </div>
   </div>
-
 </template>
 
 <script>
-// @ is an alias to /src
-import HomeSidebar from "@/components/HomeComponents/Sidebar/HomeSidebar.vue";
-import PostDisplay from "@/components/HomeComponents/PostDisplay.vue";
-import CreatePost from "@/components/HomeComponents/PostCreation/CreatePost.vue";
-import FacebookLiteFilters from "@/components/HomeComponents/FacebookLiteFilters.vue";
+  // @ is an alias to /src
+  import HomeSidebar from "@/components/HomeComponents/Sidebar/HomeSidebar.vue";
+  import PostDisplay from "@/components/HomeComponents/PostDisplay.vue";
+  import CreatePost from "@/components/HomeComponents/PostCreation/CreatePost.vue";
+  import FacebookLiteFilters from "@/components/HomeComponents/FacebookLiteFilters.vue";
+  import LoginPage from "@/components/HomeComponents/LoginComponents/LoginPage";
 
-export default {
-  name: "Home",
-  components: {
-    HomeSidebar,
-    PostDisplay,
-    FacebookLiteFilters,
-    CreatePost
-  },
-};
+  export default {
+    name: "Home",
+    components: {
+      LoginPage,
+      HomeSidebar,
+      PostDisplay,
+      FacebookLiteFilters,
+      CreatePost,
+    },
+    computed: {
+      userLoggedIn() {
+        return false;
+      },
+    },
+  };
 </script>
 
-
 <style scoped>
-.home {
-  width: 100%;
-  display: flex;
-  flex-flow: row;
-  background: rgb(244, 246, 247);
-}
+  .home {
+    width: 100%;
+    display: flex;
+    flex-flow: row;
+    background: rgb(244, 246, 247);
+  }
 
-.content {
-  display: flex;
-  flex-direction: column;
-  width: 60vw;
-  padding-top: 3vh;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  margin-left: 100px;
-}
+  .content {
+    display: flex;
+    flex-direction: column;
+    width: 60vw;
+    padding-top: 3vh;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    margin-left: 100px;
+  }
 
-.filter {
-  /* margin-left: 9vw; */
-  margin-right: 2vw;
-}
+  .filter {
+    /* margin-left: 9vw; */
+    margin-right: 2vw;
+  }
 </style>
