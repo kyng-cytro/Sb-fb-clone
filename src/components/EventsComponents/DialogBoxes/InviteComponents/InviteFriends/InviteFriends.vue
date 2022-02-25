a30183a4fb72fee3d1cfb4379b33370430e35556
lucasamonrc <lucasamonrc@gmail.com>
2022-02-25 10:20:39 -0700

    filter friends by selected list

--- src/components/EventsComponents/DialogBoxes/InviteComponents/InviteFriends/InviteFriends.vue
+++ src/components/EventsComponents/DialogBoxes/InviteComponents/InviteFriends/InviteFriends.vue
@@ -51,186 +51,206 @@
       </div>
     </div>
     <div id="friendSelectionContainer">
-      <InviteFriendsSidebar />
-      <InviteFriendsList />
+      <InviteFriendsSidebar v-on:selected="applySelectedList" v-on:event="applySelectedEvent" v-on:group="applySelectedGroup" />
+      <InviteFriendsList :friendsList="friendsList" />
     </div>
   </div>
 </template>
 
 <script>
-  import InviteFriendsList from "../InviteFriends/InviteFriendsList.vue";
-  import InviteFriendsSidebar from "../InviteFriends/InviteFriendsSidebar.vue";
-  import Friend from "@/vuex-orm_models/FriendModel.js";
-  export default {
-    data() {
-      return {
-        form: {
-          name: "",
-          imageSource: "",
-          numOfMutualFriends: null,
-        },
-        searchQuery: "",
-        buttonText: "",
-        showToolTip: false,
-      };
-    },
-    components: {
-      InviteFriendsList,
-      InviteFriendsSidebar,
-    },
-    mounted() {
-      this.$root.$on("triggerInviteFriendsTooltip", () => {
-        // Sleep for 700 miliseconds before opening popup
-        setTimeout(() => { // Note that a fat-arrow function is used here to preserve the scope of 'this' (see https://forum.vuejs.org/t/is-not-a-function/12444)
-          this.onOpen();
-        }, 700);
-      });
-    },
-    methods: {
-      onOpen() {
-        this.$refs.tooltip.$emit("open");
+import InviteFriendsList from "../InviteFriends/InviteFriendsList.vue";
+import InviteFriendsSidebar from "../InviteFriends/InviteFriendsSidebar.vue";
+import Friend from "@/vuex-orm_models/FriendModel.js";
+import GroupFriend from "@/vuex-orm_models/GroupFriendModel.js";
+import EventFriend from "@/vuex-orm_models/EventFriendModel.js";
+
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        imageSource: "",
+        numOfMutualFriends: null,
       },
-      addFriend() {
-        Friend.insert({ data: this.form });
-      },
-      emitToggleNonFacebookVisibility() {
-        this.$emit("toggleNonFacebookVisibility");
-      },
-      updateQuery() {
-        if (this.searchQuery) {
-          // Make the placeholder appear or disappear depending on whether the searchQuery has text
-          document.getElementById("placeholderText").style.display = "none";
-          this.showToolTip = false;
-        } else {
-          // Placeholder is empty
-          this.showToolTip = true;
-          document.getElementById("placeholderText").style.display =
-            "inline-block";
-        }
-        if (this.searchQuery.includes("@")) {
-          console.log("They're entering an email!");
-          this.buttonText = "Invite friend by email";
-        } else if (
-          //eslint-disable-next-line
-          /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(
-            this.searchQuery
-          )
-        ) {
-          // If phone number (see https://stackoverflow.com/questions/4338267/validate-phone-number-with-javascript)
-          this.buttonText = "Invite friend by phone number";
-          console.log("They're entering a phone number!");
-        } else {
-          this.buttonText = "";
-          console.log("They're still entering data.");
-        }
-      },
+      searchQuery: "",
+      buttonText: "",
+      showToolTip: false,
+      friendsList: Friend.all().slice(0, 100),
+    };
+  },
+  components: {
+    InviteFriendsList,
+    InviteFriendsSidebar,
+  },
+  mounted() {
+    this.$root.$on("triggerInviteFriendsTooltip", () => {
+      // Sleep for 700 miliseconds before opening popup
+      setTimeout(() => {
+        // Note that a fat-arrow function is used here to preserve the scope of 'this' (see https://forum.vuejs.org/t/is-not-a-function/12444)
+        this.onOpen();
+      }, 700);
+    });
+  },
+  methods: {
+    applySelectedList(list) {
+      if (list === 'all') {
+        this.friendsList = Friend.all();
+      } else {
+        this.friendsList = Friend.all().slice(0, 100);
+      }
     },
-  };
+    applySelectedGroup(group) {
+      const groupFriends = GroupFriend.all().find((friend) => friend.groupName === group).friends;
+      this.friendsList = Friend.findIn(groupFriends);
+    },
+    applySelectedEvent(event) {
+      const eventFriends = EventFriend.all().find((friend) => friend.eventName === event).friends;
+      this.friendsList = Friend.findIn(eventFriends);
+    },
+    onOpen() {
+      this.$refs.tooltip.$emit("open");
+    },
+    addFriend() {
+      Friend.insert({ data: this.form });
+    },
+    emitToggleNonFacebookVisibility() {
+      this.$emit("toggleNonFacebookVisibility");
+    },
+    updateQuery() {
+      if (this.searchQuery) {
+        // Make the placeholder appear or disappear depending on whether the searchQuery has text
+        document.getElementById("placeholderText").style.display = "none";
+        this.showToolTip = false;
+      } else {
+        // Placeholder is empty
+        this.showToolTip = true;
+        document.getElementById("placeholderText").style.display =
+          "inline-block";
+      }
+      if (this.searchQuery.includes("@")) {
+        console.log("They're entering an email!");
+        this.buttonText = "Invite friend by email";
+      } else if (
+        //eslint-disable-next-line
+        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(
+          this.searchQuery
+        )
+      ) {
+        // If phone number (see https://stackoverflow.com/questions/4338267/validate-phone-number-with-javascript)
+        this.buttonText = "Invite friend by phone number";
+        console.log("They're entering a phone number!");
+      } else {
+        this.buttonText = "";
+        console.log("They're still entering data.");
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .container {
-    padding: 15px;
-    padding-bottom: 0px;
-  }
+.container {
+  padding: 15px;
+  padding-bottom: 0px;
+}
 
-  #placeholderText {
-    position: absolute;
-    margin-left: 50px;
-    margin-top: 8px;
-    color: rgb(77, 72, 72);
-  }
+#placeholderText {
+  position: absolute;
+  margin-left: 50px;
+  margin-top: 8px;
+  color: rgb(77, 72, 72);
+}
 
-  #searchBoxContainer {
-    border-radius: 30px;
-    width: 600px;
-    height: 40px;
-    display: inline-block;
-    border: 0px solid #ccc;
-    box-sizing: border-box;
-    background-color: rgb(240, 242, 245);
-  }
+#searchBoxContainer {
+  border-radius: 30px;
+  width: 600px;
+  height: 40px;
+  display: inline-block;
+  border: 0px solid #ccc;
+  box-sizing: border-box;
+  background-color: rgb(240, 242, 245);
+}
 
-  #searchBox2 {
-    position: relative;
-    padding-left: 50px;
-    background: none;
-    border-radius: 30px;
-    width: 600px;
-    height: 40px;
-    display: inline-block;
-    border: 0px solid #ccc;
-    box-sizing: border-box;
-    /* background-color: rgb(240, 242, 245); */
-  }
+#searchBox2 {
+  position: relative;
+  padding-left: 50px;
+  background: none;
+  border-radius: 30px;
+  width: 600px;
+  height: 40px;
+  display: inline-block;
+  border: 0px solid #ccc;
+  box-sizing: border-box;
+  /* background-color: rgb(240, 242, 245); */
+}
 
-  #searchBox2:focus {
-    outline: 0px;
-  }
+#searchBox2:focus {
+  outline: 0px;
+}
 
-  #searchBox {
-    border-radius: 30px;
-    width: 600px;
-    height: 40px;
-    display: inline-block;
-    border: 0px solid #ccc;
-    box-sizing: border-box;
-    background-color: rgb(240, 242, 245);
-  }
+#searchBox {
+  border-radius: 30px;
+  width: 600px;
+  height: 40px;
+  display: inline-block;
+  border: 0px solid #ccc;
+  box-sizing: border-box;
+  background-color: rgb(240, 242, 245);
+}
 
-  #searchBox:focus {
-    outline: 0px;
-  }
+#searchBox:focus {
+  outline: 0px;
+}
 
-  .input-icons i {
-    position: absolute;
-  }
+.input-icons i {
+  position: absolute;
+}
 
-  .inner {
-    position: relative;
-    height: auto;
-  }
+.inner {
+  position: relative;
+  height: auto;
+}
 
-  .full-height {
-    height: 100%;
-  }
+.full-height {
+  height: 100%;
+}
 
-  .input-icons {
-    width: 100%;
-    margin-bottom: 10px;
-  }
+.input-icons {
+  width: 100%;
+  margin-bottom: 10px;
+}
 
-  .icon {
-    padding: 10px;
-    min-width: 50px;
-    text-align: center;
-  }
+.icon {
+  padding: 10px;
+  min-width: 50px;
+  text-align: center;
+}
 
-  .input-field {
-    width: 100%;
-    padding: 10px;
-    text-align: left;
-    /* Below makes it so that there's room for the search bar icon */
-    padding-left: 43px;
-    font-size: 0.9em;
-  }
+.input-field {
+  width: 100%;
+  padding: 10px;
+  text-align: left;
+  /* Below makes it so that there's room for the search bar icon */
+  padding-left: 43px;
+  font-size: 0.9em;
+}
 
-  #searchBox {
-    width: 370px;
-  }
+#searchBox {
+  width: 370px;
+}
 
-  #friendSelectionContainer {
-    display: flex;
-  }
-  button {
-    margin-left: 12px;
-  }
-  #nonFacebookButton {
-    margin-left: 20px;
-  }
-  /* TODO: adjust the placeholder text such that it looks right */
+#friendSelectionContainer {
+  display: flex;
+}
+button {
+  margin-left: 12px;
+}
+#nonFacebookButton {
+  margin-left: 20px;
+}
+/* TODO: adjust the placeholder text such that it looks right */
 
-  /* #searchBox::placeholder {
+/* #searchBox::placeholder {
     padding-left: 30px;
 }
 
--- src/components/EventsComponents/DialogBoxes/InviteComponents/InviteFriends/InviteFriendsList.vue
+++ src/components/EventsComponents/DialogBoxes/InviteComponents/InviteFriends/InviteFriendsList.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="container">
     <div>
-      <button v-if="!selectAll" id="selectAll" class="selectRectangle" @click="selectAllFriends">
+      <button
+        v-if="!selectAll"
+        id="selectAll"
+        class="selectRectangle"
+        @click="selectAllFriends"
+      >
         Select All
       </button>
-      <button v-else id="selectAll" class="selectRectangle" @click="deselectAllFriends">
-        Deselct All
+      <button
+        v-else
+        id="selectAll"
+        class="selectRectangle"
+        @click="deselectAllFriends"
+      >
+        Deselect All
       </button>
-      <div v-for="friend in friends" :key="friend.id" class="selectRectangle">
+      <div
+        v-for="friend in friendsList"
+        :key="friend.id"
+        class="selectRectangle"
+      >
         <InviteFriendsListItem v-bind:friend="friend" />
       </div>
     </div>
@@ -15,70 +29,65 @@
 </template>
 
 <script>
-  import InviteFriendsListItem from "./InviteFriendsListItem.vue";
-  import Friend from "@/vuex-orm_models/FriendModel.js";
-  import { FriendPopulation } from "@/mixins/FriendPopulation.js";
+import InviteFriendsListItem from "./InviteFriendsListItem.vue";
+import { FriendPopulation } from "@/mixins/FriendPopulation.js";
 
-  export default {
-    mixins: [FriendPopulation],
-    components: {
-      InviteFriendsListItem,
+export default {
+  mixins: [FriendPopulation],
+  components: {
+    InviteFriendsListItem,
+  },
+  props: ["friendsList"],
+  data() {
+    return {
+      selectedFriends: [],
+      selectAll: false,
+    };
+  },
+  methods: {
+    selectAllFriends() {
+      this.selectOrDeselectAllFriends(true);
+      this.selectAll = true;
     },
-    data() {
-      return {
-        selectedFriends: [],
-        selectAll: false,
-      };
+    deselectAllFriends() {
+      this.selectOrDeselectAllFriends(false);
+      this.selectAll = false;
     },
-    methods: {
-      selectAllFriends() {
-        this.selectOrDeselectAllFriends(true);
-        this.selectAll = true;
-      },
-      deselectAllFriends() {
-        this.selectOrDeselectAllFriends(false);
-        this.selectAll = false;
-      },
-    },
-    computed: {
-      friends() {
-        return Friend.all();
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style scoped>
-  .container {
-    width: 350px;
-    height: 350px;
-    overflow-x: hidden;
-    overflow-y: auto;
-  }
-  .selectRectangle {
-    background-color: transparent;
-    border-radius: 8px;
-    border: 0px;
-    display: inline-block;
-    cursor: pointer;
-    text-decoration: none;
-    width: 100%;
-    padding: 8px;
-    height: 60px;
-  }
+.container {
+  width: 350px;
+  height: 350px;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+.selectRectangle {
+  background-color: transparent;
+  border-radius: 8px;
+  border: 0px;
+  display: inline-block;
+  cursor: pointer;
+  text-decoration: none;
+  width: 100%;
+  padding: 8px;
+  height: 60px;
+}
 
-  #selectAll {
-    color: rgb(24, 113, 230);
-    font-family: Arial;
-    font-size: 17px;
-    font-weight: bold;
-    padding: 16px 31px;
-  }
+#selectAll {
+  color: rgb(24, 113, 230);
+  font-family: Arial;
+  font-size: 17px;
+  font-weight: bold;
+  padding: 16px 31px;
+}
 
-  .selectRectangle:hover {
-    background-color: rgb(242, 242, 242);
-  }
-  /* .selectRectangle:active {
+.selectRectangle:hover {
+  background-color: rgb(242, 242, 242);
+}
+/* .selectRectangle:active {
 	position:relative;
 	top:1px;
 } */
