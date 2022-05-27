e5aa7ace357b72e4fce683215cf9d2faadfd73bf
Braeden Kilburn <braedenk99@gmail.com>
2022-05-27 12:19:19 -0600

    Made spacing more uniform everywhere

--- src/components/HomeComponents/PostSquare.vue
+++ src/components/HomeComponents/PostSquare.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="postSquare">
     <div class="header">
-      <FriendDisplay :friend="post.friend" :bold="true" :slotBelowText="true"/>
-      <p id="date">{{ formatDateNoYear(post.date) }}</p>
+      <FriendDisplay
+        :friend="post.friend"
+        :bold="true"
+        :slotBelowText="true"
+        :addDate="formatDateNoYear(post.date)"
+      />
     </div>
 
     <p id="text">{{ post.text }}</p>
@@ -37,7 +41,10 @@
           </button>
         </div>
         <div>
-          <button type="button" class="btn btn-secondary d-flex align-items-center">
+          <button
+            type="button"
+            class="btn btn-secondary d-flex align-items-center"
+          >
             <i class="bi bi-chat"></i>
             <span class="pl-2">Comment</span>
           </button>
@@ -54,163 +61,153 @@
 </template>
 
 <script>
-  import FacebookLite from "@/vuex-orm_models/FacebookLiteModel.js";
-  import FriendDisplay from "@/components/Multipurpose/FriendDisplay";
-  import { dateProcessing } from "@/mixins/DateProcessing.js";
-  import FiltersValues from "@/vuex-orm_models/FilterModel.js";
+import FacebookLite from "@/vuex-orm_models/FacebookLiteModel.js";
+import FriendDisplay from "@/components/Multipurpose/FriendDisplay";
+import { dateProcessing } from "@/mixins/DateProcessing.js";
+import FiltersValues from "@/vuex-orm_models/FilterModel.js";
 
-  export default {
-    name: "post-square",
-    components: {
-      FriendDisplay,
+export default {
+  name: "post-square",
+  components: {
+    FriendDisplay,
+  },
+  props: ["post"],
+  mixins: [dateProcessing],
+  computed: {
+    isFacebookLite() {
+      return FacebookLite.find(1).enabled;
     },
-    props: ["post"],
-    mixins: [dateProcessing],
-    computed: {
-      isFacebookLite() {
-        return FacebookLite.find(1).enabled;
-      },
-      isUserViewOnly() {
-        return FiltersValues.find(1).isUserViewOnly;
-      },
-      isViewOnly() {
-        // it's view only if it's not facebook light and the post is view only, or it's not UserViewonly and it is postViewOnly
-        // In the boolean logic below, putting isFacebookLite first prevents a null access exception due to short circuit evaluation
-        return (
-          (!this.isFacebookLite || !this.isUserViewOnly) &&
-          !this.post.filter.isPostViewOnly
-        );
-      },
+    isUserViewOnly() {
+      return FiltersValues.find(1).isUserViewOnly;
     },
-  };
+    isViewOnly() {
+      // it's view only if it's not facebook light and the post is view only, or it's not UserViewonly and it is postViewOnly
+      // In the boolean logic below, putting isFacebookLite first prevents a null access exception due to short circuit evaluation
+      return (
+        (!this.isFacebookLite || !this.isUserViewOnly) &&
+        !this.post.filter.isPostViewOnly
+      );
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .postSquare {
-    background-color: white;
-    width: 40vw;
-    border-radius: 10px;
-    box-shadow: 0px 0px 3px rgb(140, 140, 140);
-    margin-top: 2%;
-    margin-bottom: 3%;
-    display: flex;
-    flex-direction: column;
-  }
+.postSquare {
+  background-color: white;
+  width: 40vw;
+  border-radius: 10px;
+  box-shadow: 0px 0px 3px rgb(140, 140, 140);
+  margin-top: 2%;
+  margin-bottom: 3%;
+  display: flex;
+  flex-direction: column;
+}
 
-  .postPic {
-    width: 40vw;
-    object-fit: cover; /*This makes it so the image is cropped instead of squished */
-  }
-  .postPicNoButtons {
-    width: 40vw;
-    /* height: 40vh; */
-    object-fit: cover; /*This makes it so the image is cropped instead of squished */
-    border-radius: 0px 0px 10px 10px;
-  }
+.postPic {
+  width: 40vw;
+  object-fit: cover; /*This makes it so the image is cropped instead of squished */
+}
+.postPicNoButtons {
+  width: 40vw;
+  /* height: 40vh; */
+  object-fit: cover; /*This makes it so the image is cropped instead of squished */
+  border-radius: 0px 0px 10px 10px;
+}
 
-  .info {
-    margin: 10px;
-  }
+.info {
+  margin: 10px;
+}
 
-  #name:hover {
-    text-decoration: underline;
-  }
+#name:hover {
+  text-decoration: underline;
+}
 
-  .Buttons {
-    display: flex;
-    flex-direction: row;
-    flex: 33%;
-    justify-content: space-between;
-  }
+.Buttons {
+  display: flex;
+  flex-direction: row;
+  flex: 33%;
+  justify-content: space-between;
+}
 
-  button {
-    padding-left: 13px;
-    padding-right: 13px;
-    width: 120px;
-    margin: 2%;
-    font-weight: bold;
-    font-size: 0.9em;
-  }
+button {
+  padding-left: 13px;
+  padding-right: 13px;
+  width: 120px;
+  margin: 2%;
+  font-weight: bold;
+  font-size: 0.9em;
+}
 
-  .btn-secondary {
-    color: rgb(78, 78, 78);
-    background-color: transparent;
-    border: 0px;
-    border-radius: 6px;
-    margin: 2%;
-    font-weight: bold;
-    font-size: 0.9em;
-  }
+.btn-secondary {
+  color: rgb(78, 78, 78);
+  background-color: transparent;
+  border: 0px;
+  border-radius: 6px;
+  margin: 2%;
+  font-weight: bold;
+  font-size: 0.9em;
+}
 
-  .btn-secondary:hover {
-    color: rgb(78, 78, 78);
-    background-color: hsl(0, 0%, 92%);
-    transition: 0.3s;
-  }
+.btn-secondary:hover {
+  color: rgb(78, 78, 78);
+  background-color: hsl(0, 0%, 92%);
+  transition: 0.3s;
+}
 
-  .btn-secondary:focus {
-    outline-color: transparent;
-    border: none;
-  }
+.btn-secondary:focus {
+  outline-color: transparent;
+  border: none;
+}
 
-  #data {
-    color: rgb(111, 111, 111);
-    font-size: 0.9em;
-  }
+#data {
+  color: rgb(111, 111, 111);
+  font-size: 0.9em;
+}
 
-  #date {
-    margin-left: 42px;
-    margin-top: -5px;
-    font-size: 0.75em;
-    font-weight: bold;
-    color: rgb(136, 136, 136);
-  }
+em {
+  color: rgb(111, 111, 111);
+  font-size: 0.9em;
+}
 
-  em {
-    color: rgb(111, 111, 111);
-    font-size: 0.9em;
-  }
+.header {
+  padding: 15px;
+  flex-direction: column;
+}
 
-  .header {
-    padding: 15px;
-    flex-direction: column;
-  }
+#nameHeader {
+  text-align: center;
+  margin: 10px;
+}
 
-  #nameHeader {
-    text-align: center;
-    margin: 10px;
-  }
+#personPic {
+  flex-direction: inherit;
+  margin: 10px;
+  height: 25px;
+  width: 25px;
 
-  #personPic {
-    flex-direction: inherit;
-    margin: 10px;
-    height: 25px;
-    width: 25px;
+  background-color: #bbb;
+}
 
-    background-color: #bbb;
-  }
+.line {
+  border-bottom: 1px solid #bbb;
+  padding: 3px;
+}
 
-  .line {
-    border-bottom: 1px solid #bbb;
-    padding: 3px;
-  }
+p#text {
+  margin-left: 10px;
+  margin-right: 10px;
+}
 
-  p#text {
-    /*margin:10px;*/
-    margin-top: -20px;
-    margin-left: 10px;
-    margin-right: 10px;
-  }
+.icon-flipped {
+  transform: scaleX(-1);
+  -moz-transform: scaleX(-1);
+  -webkit-transform: scaleX(-1);
+  -ms-transform: scaleX(-1);
+}
 
-  .icon-flipped {
-    transform: scaleX(-1);
-    -moz-transform: scaleX(-1);
-    -webkit-transform: scaleX(-1);
-    -ms-transform: scaleX(-1);
-  }
-
-  i {
-    font-size: 1.3em;
-    margin: 2px;
-  }
+i {
+  font-size: 1.3em;
+  margin: 2px;
+}
 </style>
--- src/components/Multipurpose/FriendDisplay.vue
+++ src/components/Multipurpose/FriendDisplay.vue
@@ -28,13 +28,14 @@
     ></i>
 
     <!-- The name of the person -->
-    <div v-if="slotBelowText == null || !slotBelowText">
-      <p v-show="!onlyImage" :style="textStyles">{{ friend.name }}</p>
-    </div>
+    <p v-if="slotBelowText == null || slotBelowText == false" :style="textStyles">
+      {{ friend.name }}
+    </p>
     <!-- Or, if they include another element in this component, it shows up here -->
-    <div v-else-if="slotBelowText" class="textAndSlot">
-      <div>
-        <p v-show="!onlyImage" :style="textStyles">{{ friend.name }}</p>
+    <div v-else-if="slotBelowText == true" class="textAndSlot">
+      <div class="d-flex flex-column">
+        <p v-show="!onlyImage" :style="textStyles" class="m-0">{{ friend.name }}</p>
+        <p v-if="addDate != null" id="date">{{addDate}}</p>
         <slot></slot>
       </div>
     </div>
@@ -42,152 +43,151 @@
 </template>
 
 <script>
-  export default {
-    // friend is a friend object
-    // size is a value of 'small' or 'normal'
-    props: ["friend", "size", "onlyImage", "bold", "slotBelowText"],
-    computed: {
-      isFacebookFriend() {
-        // Assumed to be a Facebook friend if the friend object has an image source
-        return Object.prototype.hasOwnProperty.call(this.friend, "imageSource");
-      },
-      isFacebookWithImage() {
-        return this.isFacebookFriend && this.friend.imageSource !== "";
-      },
-      isFacebookWithoutImage() {
-        return (
-          !this.isFacebookWithImage &&
-          !this.isEmail &&
-          !this.isPhone &&
-          !this.isBoth
-        );
-      },
-      isEmail() {
-        return (
-          Object.prototype.hasOwnProperty.call(this.friend, "email") &&
-          this.friend.email !== "" &&
-          !(this.friend.phone !== "")
-        );
-      },
-      isPhone() {
-        return (
-          Object.prototype.hasOwnProperty.call(this.friend, "phone") &&
-          this.friend.phone !== "" &&
-          !(this.friend.email !== "")
-        );
-      },
-      isBoth() {
-        return (
-          !this.isFacebookFriend &&
-          this.friend.phone !== "" &&
-          this.friend.email !== ""
-        );
-      },
+export default {
+  // friend is a friend object
+  // size is a value of 'small' or 'normal'
+  props: ["friend", "size", "onlyImage", "bold", "slotBelowText", "addDate"],
+  computed: {
+    isFacebookFriend() {
+      // Assumed to be a Facebook friend if the friend object has an image source
+      return Object.prototype.hasOwnProperty.call(this.friend, "imageSource");
+    },
+    isFacebookWithImage() {
+      return this.isFacebookFriend && this.friend.imageSource !== "";
+    },
+    isFacebookWithoutImage() {
+      return (
+        !this.isFacebookWithImage &&
+        !this.isEmail &&
+        !this.isPhone &&
+        !this.isBoth
+      );
+    },
+    isEmail() {
+      return (
+        Object.prototype.hasOwnProperty.call(this.friend, "email") &&
+        this.friend.email !== "" &&
+        !(this.friend.phone !== "")
+      );
+    },
+    isPhone() {
+      return (
+        Object.prototype.hasOwnProperty.call(this.friend, "phone") &&
+        this.friend.phone !== "" &&
+        !(this.friend.email !== "")
+      );
+    },
+    isBoth() {
+      return (
+        !this.isFacebookFriend &&
+        this.friend.phone !== "" &&
+        this.friend.email !== ""
+      );
+    },
 
-      imgStyles() {
-        if (this.size === "small") {
-          return `
-                    margin-top: 5px;
+    imgStyles() {
+      if (this.size === "small") {
+        return `
                     width: 25px;
                     height: 25px;`;
-        } else {
-          return `
-                    margin-top: 5px;
+      } else {
+        return `
                     width: 37px;
                     height: 37px;`;
-        }
-      },
+      }
+    },
 
-      textStyles() {
-        let styles = "";
-        if (this.size === "small") {
-          styles += `
-                        margin-left: 3px;
+    textStyles() {
+      let styles = "";
+      if (this.size === "small") {
+        styles += `
                         font-size: 0.9em;;
-                        width: 120px;
                         text-align: left;
-                        padding-top: 10px;
-                        line-height: 15px;
-                        word-wrap:break-word;`;
-          if (this.friend.name.length > 15) { // Chances are that it had to wrap around (although not for sure)
-            styles += "padding-top: 2px;";
-          }
-        } else {
-          styles += `
-                        margin-left: 10px;
-                        margin-top: 7px;
-                        font-size: 1em;`;
+                        margin: 0;
+                        margin-left: 10px;`;
+        if (this.friend.name.length > 15) {
+          // Chances are that it had to wrap around (although not for sure)
+          styles += "padding-top: 2px;";
         }
-        if (this.bold) {
-          styles += `
+      } else {
+        styles += `
+                        margin: 0;
+                        margin-left: 10px;
+                        font-size: 1em;`;
+      }
+      if (this.bold) {
+        styles += `
+                        line-height: normal;
                         font-weight: bold;
                         `;
-        }
-        if (this.slotBelowText) {
-          // If there's something below the text, we need to move the text up a bit.
-          styles += `
+      }
+      if (this.slotBelowText) {
+        // If there's something below the text, we need to move the text up a bit.
+        styles += `
                 font-size: 0.9em;
                 margin-top: 0%;
                 margin-bottom: -2%;
             `;
-        }
-        return styles;
-      },
+      }
+      return styles;
+    },
 
-      iconStyles() {
-        let styles = "";
-        if (this.size === "small") {
-          styles += `
+    iconStyles() {
+      let styles = "";
+      if (this.size === "small") {
+        styles += `
                         padding-top: 2px;
                         font-size: 1.6em;
                         `;
-        } else {
-          styles += `
-                        padding-top: 6px;
+      } else {
+        styles += `
                         font-size: 2em;`;
-        }
-        return styles;
-      },
-    },
-    methods: {
-      setAltImg(event) {
-        event.target.src = `https://ui-avatars.com/api/?name=${this.friend.name.split(' ').join('+')}`;
       }
-    }
-  };
+      return styles;
+    },
+  },
+  methods: {
+    setAltImg(event) {
+      event.target.src = `https://ui-avatars.com/api/?name=${this.friend.name
+        .split(" ")
+        .join("+")}`;
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .horizontalAlign {
-    display: flex;
-    padding: 0px;
-    height: 30px;
-    line-height: 35px;
-    text-align: flex-start;
-  }
+.horizontalAlign {
+  display: flex;
+  align-items: center;
+  padding: 0px;
+  line-height: 35px;
+  text-align: flex-start;
+}
 
-  img {
-    object-fit: cover; /*This makes it so the image is cropped instead of squished */
-    border-radius: 50%;
-  }
+img {
+  object-fit: cover; /*This makes it so the image is cropped instead of squished */
+  border-radius: 50%;
+}
 
-  p i {
-    padding-left: -15px;
-  }
+p i {
+  padding-left: -15px;
+}
 
-  i {
-    /* padding-right: 60px; */
-    color: rgb(137, 142, 149);
-    font-weight: bold;
-  }
-  .textAndSlot {
-    /* width: fit-content; */
-    /* width: 100%; */
-    margin-left: 0px;
-    padding-left: 0px;
-    margin-right: auto;
-    height: fit-content;
-    width: 100%;
-    text-align: left;
-  }
+i {
+  /* padding-right: 60px; */
+  color: rgb(137, 142, 149);
+  font-weight: bold;
+}
+.textAndSlot {
+  margin-left: 10px;
+}
+
+#date {
+  margin: 0;
+  line-height: normal;
+  font-size: 0.75em;
+  font-weight: bold;
+  color: rgb(136, 136, 136);
+}
 </style>
